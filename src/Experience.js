9c517dca6d4b73ff2c104bba4a57d421bc38e090
Oleksii Bardyshev <oleksii.bardyshev@unilimes.com>
2022-11-23 13:49:24 +0200

    Initial commit

---
+++ src/Experience.js
@@ -0,0 +1,32 @@
+import {Center, OrbitControls, Text3D, useMatcapTexture} from '@react-three/drei'
+import { Perf } from 'r3f-perf'
+
+export default function Experience() {
+  const matcapTexture = useMatcapTexture('7B5254_E9DCC7_B19986_C8AC91', 256)
+
+    return <>
+
+        <Perf position="top-left" />
+
+        <OrbitControls makeDefault />
+
+        <Center>
+          <Text3D
+            font="./fonts/helvetiker_regular.typeface.json"
+            size={0.75}
+            height={0.2}
+            curveSegments={12}
+            bevelEnabled
+            bevelThickness={0.02}
+            bevelSize={0.02}
+            bevelOffset={0}
+            bevelSegments={5}
+
+          >
+            oleksii.dev
+            <meshNormalMaterial />
+          </Text3D>
+        </Center>
+
+    </>
+}
