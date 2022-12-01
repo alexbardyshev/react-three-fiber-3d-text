9324a3ea64c699785561c9b69ca05c1f738a38ae
Oleksii Bardyshev <oleksii.bardyshev@unilimes.com>
2022-12-01 15:38:05 +0200

    Donuts were created

--- src/Experience.js
+++ src/Experience.js
@@ -2,7 +2,12 @@ import {Center, OrbitControls, Text3D, useMatcapTexture} from '@react-three/drei
 import { Perf } from 'r3f-perf'
 
 export default function Experience() {
-  const matcapTexture = useMatcapTexture('7B5254_E9DCC7_B19986_C8AC91', 256)
+  const [ matcapTexture ] = useMatcapTexture('7B5254_E9DCC7_B19986_C8AC91', 256)
+
+  // const tempArray = [...Array(100)]
+  // tempArray.map(() => {
+  //
+  // })
 
     return <>
 
@@ -23,10 +28,30 @@ export default function Experience() {
             bevelSegments={5}
 
           >
-            oleksii.dev
-            <meshNormalMaterial />
+            OLEKSII.DEV
+            <meshMatcapMaterial matcap={ matcapTexture } />
           </Text3D>
         </Center>
+
+      { [...Array(100)].map((value, index) =>
+        <mesh
+          key={index}
+          position={[
+            (Math.random() - 0.5) * 10,
+            (Math.random() - 0.5) * 10,
+            (Math.random() - 0.5) * 10,
+          ]}
+          scale={ 0.2 + Math.random() * 0.2}
+          rotation={[
+            Math.random() + Math.PI,
+            Math.random() + Math.PI,
+            0
+          ]}
+        >
+          <torusGeometry args={[1, 0.6, 16, 32]}/>
+          <meshMatcapMaterial matcap={ matcapTexture } />
+        </mesh>
+      ) }
 
     </>
 }
