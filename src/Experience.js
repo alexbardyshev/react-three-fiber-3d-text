0d249e831c2a67bd1bffb259effad6c046896371
Oleksii Bardyshev <oleksii.bardyshev@unilimes.com>
2022-12-26 10:31:47 +0200

    Ref was added to geometry

--- src/Experience.js
+++ src/Experience.js
@@ -1,57 +1,58 @@
 import {Center, OrbitControls, Text3D, useMatcapTexture} from '@react-three/drei'
-import { Perf } from 'r3f-perf'
+import {Perf} from 'r3f-perf'
+import {useState} from "react";
 
 export default function Experience() {
-  const [ matcapTexture ] = useMatcapTexture('7B5254_E9DCC7_B19986_C8AC91', 256)
+  const [matcapTexture] = useMatcapTexture('7B5254_E9DCC7_B19986_C8AC91', 256)
 
-  // const tempArray = [...Array(100)]
-  // tempArray.map(() => {
-  //
-  // })
+  const [torusGeometry, setTorusGeometry] = useState();
 
-    return <>
+  return <>
 
-        <Perf position="top-left" />
+    <Perf position="top-left"/>
 
-        <OrbitControls makeDefault />
+    <OrbitControls makeDefault/>
 
-        <Center>
-          <Text3D
-            font="./fonts/helvetiker_regular.typeface.json"
-            size={0.75}
-            height={0.2}
-            curveSegments={12}
-            bevelEnabled
-            bevelThickness={0.02}
-            bevelSize={0.02}
-            bevelOffset={0}
-            bevelSegments={5}
+    <torusGeometry ref={ setTorusGeometry } args={[1, 0.6, 16, 32]}/>
 
-          >
-            OLEKSII.DEV
-            <meshMatcapMaterial matcap={ matcapTexture } />
-          </Text3D>
-        </Center>
+    <Center>
+      <Text3D
+        font="./fonts/helvetiker_regular.typeface.json"
+        size={0.75}
+        height={0.2}
+        curveSegments={12}
+        bevelEnabled
+        bevelThickness={0.02}
+        bevelSize={0.02}
+        bevelOffset={0}
+        bevelSegments={5}
 
-      { [...Array(100)].map((value, index) =>
-        <mesh
-          key={index}
-          position={[
-            (Math.random() - 0.5) * 10,
-            (Math.random() - 0.5) * 10,
-            (Math.random() - 0.5) * 10,
-          ]}
-          scale={ 0.2 + Math.random() * 0.2}
-          rotation={[
-            Math.random() + Math.PI,
-            Math.random() + Math.PI,
-            0
-          ]}
-        >
-          <torusGeometry args={[1, 0.6, 16, 32]}/>
-          <meshMatcapMaterial matcap={ matcapTexture } />
-        </mesh>
-      ) }
+      >
+        OLEKSII.DEV
+        <meshMatcapMaterial matcap={matcapTexture}/>
+      </Text3D>
+    </Center>
 
-    </>
+    {[...Array(100)].map(( value, index ) =>
+      <mesh
+        key={index}
+        geometry={ torusGeometry }
+        position={[
+          (Math.random() - 0.5) * 10,
+          (Math.random() - 0.5) * 10,
+          (Math.random() - 0.5) * 10,
+        ]}
+        scale={0.2 + Math.random() * 0.2}
+        rotation={[
+          Math.random() + Math.PI,
+          Math.random() + Math.PI,
+          0
+        ]}
+      >
+
+        <meshMatcapMaterial matcap={matcapTexture}/>
+      </mesh>
+    )}
+
+  </>
 }
